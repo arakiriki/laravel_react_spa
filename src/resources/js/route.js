b08f36aa275a29a8c895389cc443ca2f9ec907c4
riki-araki <riki.araki@epkotsoftware.co.jp>
2022-08-13 20:04:44 +0900

    編集機能と削除機能の実装

--- src/resources/js/route.js
+++ src/resources/js/route.js
@@ -7,6 +7,7 @@ import {
   } from 'react-router-dom';
 import Example from './pages/Example';
 import Home from './pages/Home';
+import PostEdit from './pages/PostEdit';
 
   function App() {
     return (
@@ -14,6 +15,7 @@ import Home from './pages/Home';
           <Routes>
               <Route path='/example' element={<Example/>} />
               <Route path='/' element={<Home/>} />
+              <Route path='/post/edit/:id' element={<PostEdit/>} />
           </Routes>
         </div>
     );
--- src/routes/api.php
+++ src/routes/api.php
@@ -21,4 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::group(['middleware' => 'api'], function(){
     Route::get('posts', 'App\Http\Controllers\Api\PostController@index');
     Route::post('posts/create', 'App\Http\Controllers\Api\PostController@create');
+    Route::post('edit', 'App\Http\Controllers\Api\PostController@edit'); 
+    Route::post('update', 'App\Http\Controllers\Api\PostController@update'); 
+    Route::post('delete', 'App\Http\Controllers\Api\PostController@delete');
 });
